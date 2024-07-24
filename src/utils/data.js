453f06e2a22b2ed88b37fe972f21544478a14390
Emre Uslu <emreuslu256@gmail.com>
2024-07-24 17:21:18 +0300

    sync

---
+++ src/utils/data.js
@@ -0,0 +1,99 @@
+export const teams = [
+    {
+      id: 1,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Alpha',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Alpha açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 2,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Beta',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Beta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 3,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Gamma',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Gamma açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 4,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Delta',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Delta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 5,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Epsilon',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Epsilon açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 6,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Zeta',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Zeta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 7,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Eta',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Eta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+    {
+      id: 8,
+      logo: 'https://via.placeholder.com/100',
+      name: 'Ekip Theta',
+      descshort: 'Kısa Açıklama',
+      description: 'Ekip Theta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      projects: [
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+        'x projesi | x projesi detayları ve açıklamaları',
+      ],
+    },
+  ];
+  
